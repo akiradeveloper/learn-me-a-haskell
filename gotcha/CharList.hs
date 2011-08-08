b8e0fbc87029a0e8066ea13964013e638e4abf17
Akira Hayakawa <ruby.wktk@gmail.com>
2011-08-08 19:45:55 +0900

    reflected @thimura 's advice

--- gotcha/CharList.hs
+++ gotcha/CharList.hs
@@ -1,7 +1,12 @@
+import Data.Monoid
 newtype CharList = CharList { getCharList :: [Char] }
-  deriving (Show)
+  deriving (Monoid, Show)
 
 main = do
   print $ CharList "akira"
   -- Unable to compile. How can I do this without defining CharList ++ CharList ?
   -- print $ CharList "akira" ++ CharList "developer"
+ 
+  -- @thimura told me it is impossible. he said try GeneralizedNewtypeDeriving.
+  -- Then I got an idea to make it monoid to use mappend function instead of (++) function.
+  print $ CharList "akira" `mappend` CharList "developer"
